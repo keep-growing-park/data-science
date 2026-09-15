0946a6d36f7fdf223d75f71bbc9bdd3d0e0c8a8e
keep-growing-park <keepgrowing.park@gmail.com>
2026-09-15 19:59:49 +0900

    Update home.py

--- home.py
+++ home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.title("데이터 과학 수업 자료")
-st.write("아래 버튼을 눌러 원하는 차시로 이동하세요.")
+
 st.button("1년치 박스오피스 데이터", use_container_width=True):
 st.switch_page("pages/01_one_year_boxoffice_data.py")
